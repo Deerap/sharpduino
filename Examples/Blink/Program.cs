86c340a21e06f3da151e53b92848d1c7f694e16d
pradeeppotdar <deerap@live.in>
2015-08-01 20:29:47 +0530

    Initial Commit.

---
+++ Examples/Blink/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sharpduino;
+using System.Threading;
+
+namespace Blink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Windows serial port
+            var arduino = new ArduinoUno("COM4");
+
+            arduino.SetPinMode(Sharpduino.Constants.ArduinoUnoPins.D13, Sharpduino.Constants.PinModes.Output);
+
+            while (true)
+            {
+                arduino.SetDO(Sharpduino.Constants.ArduinoUnoPins.D13, true);
+                Thread.Sleep(1000);
+                arduino.SetDO(Sharpduino.Constants.ArduinoUnoPins.D13, false);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# sharpduino
+# A .NET library for controlling an arduino through the firmata protocol
